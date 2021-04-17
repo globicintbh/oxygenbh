03d514c6595ac747453ca6e8b995aac4fb290372
Ahemd Almutawa <globicint2@gmail.com>
2021-04-18 01:28:01 +0300

    second

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API Ahmed</h1>
+      <h1>Create React App + Go API Ahmed 2</h1>
       <h2>
         Deployed with{' '}
         <a
